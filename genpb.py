376305b8c6ba457ec19451835b0580210aa0a7b0
Shubham Singh <Shubham.Singh@softnautics.com>
2021-03-15 14:44:41 +0530

    Fixed genpb script
    Signed-off-by: Shubham Singh <Shubham.Singh@softnautics.com>

--- genpb.py
+++ genpb.py
@@ -13,7 +13,129 @@ from google.protobuf import text_format as pbtf
 from tensorflow.python.tools import optimize_for_inference_lib
 from tensorflow.python.tools import freeze_graph
 import argparse
+import numpy as np
+
+from utils import Size, Point, Overlap, Score, Box, prop2abs, normalize_box
+from collections import namedtuple, defaultdict
+from math import sqrt, log, exp
+import argparse
+import math
+
+import multiprocessing as mp
+from average_precision import APCalculator, APs2mAP
+from training_data import TrainingData
+from ssdutils import get_anchors_for_preset, decode_boxes, suppress_overlaps
+from ssdmv2 import SSDMV2
+from utils import *
+from tqdm import tqdm
+import config
 import glob
+
+
+if sys.version_info[0] < 3:
+    print("This is a Python 3 program. Use Python 3 or higher.")
+    sys.exit(1)
+
+
+SSDMap = namedtuple('SSDMap', ['size', 'scale', 'aspect_ratios'])
+SSDPreset = namedtuple('SSDPreset', ['name', 'image_size', 'maps',
+                                     'extra_scale', 'num_anchors'])
+
+
+SSD_PRESETS = {
+    'mv2': SSDPreset(name = 'mv2',
+                        image_size = Size(224, 224),
+                        maps = [
+                            SSDMap(Size(14, 14), 0.2,   [2, 3, 0.5, 1./3.]),
+                            SSDMap(Size(7, 7), 0.375, [2, 3, 0.5, 1./3.]),
+                            SSDMap(Size( 4, 4), 0.55,  [2, 3, 0.5, 1./3.]),
+                            SSDMap(Size( 2,  2), 0.725, [2, 0.5])
+                        ],
+                        extra_scale = 1.075,
+                        num_anchors = 1582),}
+
+preset = SSDPreset(name='mv2', image_size=Size(w=224, h=224), maps=[SSDMap(size=Size(w=14, h=14), scale=0.2, aspect_ratios=[2, 3, 0.5, 0.3333333333333333]), SSDMap(size=Size(w=7, h=7), scale=0.375, aspect_ratios=[2, 3, 0.5, 0.3333333333333333]), SSDMap(size=Size(w=4, h=4), scale=0.55, aspect_ratios=[2, 3, 0.5, 0.3333333333333333]), SSDMap(size=Size(w=2, h=2), scale=0.725, aspect_ratios=[2, 0.5])], extra_scale=1.075, num_anchors=1582)
+
+def compute_lr(lr_values, lr_boundaries):
+    with tf.variable_scope('learning_rate'):
+        global_step = tf.Variable(0, trainable=False, name='global_step')
+        lr = tf.train.piecewise_constant(global_step, lr_boundaries, lr_values)
+    return lr, global_step
+
+
+
+def gen_pbtxt(dir,name):
+  lr_values = '0.001;0.0001;0.00001'
+  lr_boundaries = '320000;400000'
+  weight_decay = 0.0005
+  momentum = 0.9
+  # try:
+  #     td = TrainingData('pascal-voc')
+  #     print('[i] # training samples:   ', td.num_train)
+  #     print('[i] # validation samples: ', td.num_valid)
+  #     print('[i] # classes:            ', td.num_classes)
+  #     print('[i] Image size:           ', td.preset.image_size)
+  #     print(td.preset)
+  # except (AttributeError, RuntimeError) as e:
+  #     print('[!] Unable to load training data:', str(e))
+  #     # return 1
+  with tf.Session() as sess:
+    global_step = None
+    # if start_epoch == 0:
+    lr_values = lr_values.split(';')
+    try:
+        lr_values = [float(x) for x in lr_values]
+    except ValueError:
+        print('[!] Learning rate values must be floats')
+        sys.exit(1)
+
+    lr_boundaries = lr_boundaries.split(';')
+    try:
+        lr_boundaries = [int(x) for x in lr_boundaries]
+    except ValueError:
+        print('[!] Learning rate boundaries must be ints')
+        sys.exit(1)
+
+    ret = compute_lr(lr_values, lr_boundaries)
+    learning_rate, global_step = ret
+    is_training = False
+
+    net = SSDMV2(sess, preset, is_training)
+    # if start_epoch != 0:
+    #     print("hi>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+    #     net.build_from_metagraph(metagraph_file, checkpoint_file)
+    #     net.build_optimizer_from_metagraph()
+    # else:
+    net.build_from_mv2(1)
+    net.build_optimizer(learning_rate=learning_rate,
+                            global_step=global_step,
+                            weight_decay=weight_decay,
+                            momentum=momentum)
+    # if args.generate_pbtxt:
+    new_name = name+".pbtxt"
+    tf.train.write_graph(sess.graph_def, dir, new_name, as_text=True)
+
+
+
+def get_latest_ckpt(file_dir):
+  # import glob
+  # temp = []
+  # p = os.listdir("./checkpoints/")
+  # for i in p:
+  #   if ".pbtxt" in i:
+  #     temp.append(i)
+  #print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  file_name =os.path.join(file_dir,"*.meta")
+  #print(file_name,">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+  list_of_files = glob.glob(file_name)
+  #list_of_files1 = glob.glob("checkpoints/*.pbtxt")
+  #print(list_of_files)
+  latest_file = max(list_of_files, key=os.path.getmtime)
+  return latest_file.split("/")[-1].split(".")[0] 
+
+
+
+
 
 def get_platform():
   platforms = {
@@ -378,48 +500,23 @@ Refer to Readme.txt for more details.
 
 please Modify following dataPara line to process new checkpoint folder""" 
 
-
-def get_latest_pbtxt(file_dir):
-  # import glob
-  # temp = []
-  # p = os.listdir("./checkpoints/")
-  # for i in p:
-  #   if ".pbtxt" in i:
-  #     temp.append(i)
-
-  file_name ="*.pbtxt"
-  print(file_name)
-  list_of_files = glob.glob(file_name)
-  #list_of_files1 = glob.glob("checkpoints/*.pbtxt")
-  print(list_of_files)
-  latest_file = max(list_of_files, key=os.path.getmtime)
-  print(latest_file)
-  return latest_file
-
-
-
-
-
-def main(checkpoint_dir):
+def main(ckpt_name):
   indexOfModel=0
-  pbtxt = get_latest_pbtxt(checkpoint_dir)
-
-
-  
-
-
-  dataParas=[[pbtxt,      'image_input',  'image_input',  ['classifiers/classifier0_0/Conv2D','classifiers/classifier0_1/Conv2D','classifiers/classifier0_2/Conv2D','classifiers/classifier0_3/Conv2D', 'classifiers/classifier0_4/Conv2D','classifiers/classifier0_5/Conv2D', 'classifiers/classifier1_0/Conv2D','classifiers/classifier1_1/Conv2D','classifiers/classifier1_2/Conv2D','classifiers/classifier1_3/Conv2D','classifiers/classifier1_4/Conv2D','classifiers/classifier1_5/Conv2D'],    True,[1,224,224,3]]]
+  dataParas=[[ckpt_name + ".pbtxt",      'image_input',  'image_input',  ['classifiers/classifier0_0/Conv2D','classifiers/classifier0_1/Conv2D','classifiers/classifier0_2/Conv2D','classifiers/classifier0_3/Conv2D', 'classifiers/classifier0_4/Conv2D','classifiers/classifier0_5/Conv2D', 'classifiers/classifier1_0/Conv2D','classifiers/classifier1_1/Conv2D','classifiers/classifier1_2/Conv2D','classifiers/classifier1_3/Conv2D','classifiers/classifier1_4/Conv2D','classifiers/classifier1_5/Conv2D'],    True,[1,224,224,3]]]
   demoCKPT2PB(dataParas[indexOfModel])
 
 if __name__ == '__main__':
   parser = argparse.ArgumentParser()
   os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
   parser.add_argument("--ckpt_dir", type=str, required=True, default="", help="Checkpoint directory")
-  #parser.add_argument("--ckpt_name", type=str, required=True, default="", help="Checkpoint name that needs to be frozen")
+  # parser.add_argument("--ckpt_name", type=str, required=True, default="", help="Checkpoint name that needs to be frozen")
   parser.add_argument("--freeze", required = False, default = True, action = "store_true", help="Freeze graph for inference.")
   args = parser.parse_args()
+
+  ckpt_name =  get_latest_ckpt(args.ckpt_dir)
+  gen_pbtxt(args.ckpt_dir,ckpt_name)
   os.chdir(os.path.abspath(args.ckpt_dir))
   if args.freeze:
-    main(args.ckpt_dir)  
+    main(ckpt_name)  
 
 
